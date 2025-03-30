1eddbfecfbde4f34aaafb41afe3a04f99835fd73
RagexPrince683 <yellowtoad91@gmail.com>
2025-03-30 12:20:55 -0400

    Revert "fuck this shit im out"
    This reverts commit 6285e6098469bfd5fdf797d2be7bb0e70c08e521.

--- src/main/java/com/hfr/data/MarketData.java
+++ src/main/java/com/hfr/data/MarketData.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class MarketData extends WorldSavedData {
 	private static final String FILE_NAME = "marketdata.json";
@@ -46,60 +45,34 @@ public class MarketData extends WorldSavedData {
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
-		int count = nbt.getInteger("count");
-		readMarkets(nbt, count);
 		loadFromFile();
-	}
-
-	public void readMarkets(NBTTagCompound nbt, int count) {
-		for (int index = 0; index < count; index++) {
-			String name = nbt.getString("market_" + index);
-			int offerCount = nbt.getInteger("offercount_" + index);
-			for (int off = 0; off < offerCount; off++) {
-				readOffers(nbt, name, off);
-			}
-		}
-	}
-
-	public void readOffers(NBTTagCompound nbt, String name, int index) {
-		ItemStack[] slots = new ItemStack[4];
-		NBTTagList list = nbt.getTagList("items" + name + index, 10);
-		for (int j = 0; j < list.tagCount(); j++) {
-			NBTTagCompound nbt1 = list.getCompoundTagAt(j);
-			byte b0 = nbt1.getByte("slot" + index);
-			if (b0 >= 0 && b0 < slots.length) {
-				slots[b0] = ItemStack.loadItemStackFromNBT(nbt1);
-			}
-		}
-		List<ItemStack[]> existingOffers = offers.get(name);
-		if (existingOffers == null) {
-			existingOffers = new ArrayList<ItemStack[]>();
-			offers.put(name, existingOffers);
-		}
-		existingOffers.add(slots);
 	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound nbt) {
-		nbt.setInteger("count", offers.size());
-		writeMarkets(nbt);
 		saveToFile();
 	}
 
-	public void writeMarkets(NBTTagCompound nbt) {
-		int index = 0;
-		for (Entry<String, List<ItemStack[]>> entry : offers.entrySet()) {
-			nbt.setString("market_" + index, entry.getKey());
-			nbt.setInteger("offercount_" + index, entry.getValue().size());
-			writeOffers(nbt, entry.getKey(), entry.getValue());
-			index++;
-		}
+	public void writeMarketFromName(NBTTagCompound nbt, String name) {
+
+		List<ItemStack[]> market = this.offers.get(name);
+
+		if(market == null)
+			return;
+
+		nbt.setString("market", name);
+		nbt.setInteger("offercount", market.size());
+
+		writeOffers(nbt, name, market);
 	}
 
 	public void writeOffers(NBTTagCompound nbt, String name, List<ItemStack[]> offers) {
+
 		for (int index = 0; index < offers.size(); index++) {
+
 			NBTTagList list = new NBTTagList();
 			ItemStack[] offer = offers.get(index);
+
 			for (int i = 0; i < offer.length; i++) {
 				if (offer[i] != null) {
 					NBTTagCompound nbt1 = new NBTTagCompound();
@@ -107,8 +80,8 @@ public class MarketData extends WorldSavedData {
 					offer[i].writeToNBT(nbt1);
 					list.appendTag(nbt1);
 				}
+
 			}
-			nbt.setTag("items" + name + index, list);
 		}
 	}
 
