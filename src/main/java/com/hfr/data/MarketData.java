c6dbfed833d2fd8dae274bdbc5c682a8cd1e9a7d
RagexPrince683 <yellowtoad91@gmail.com>
2025-03-30 11:57:24 -0400

    worst code ever award

--- src/main/java/com/hfr/data/MarketData.java
+++ src/main/java/com/hfr/data/MarketData.java
@@ -1,19 +1,29 @@
 package com.hfr.data;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldSavedData;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MarketData extends WorldSavedData {
-	
-	public HashMap<String, List<ItemStack[]>> offers = new HashMap();
+	private static final String FILE_NAME = "marketdata.json";
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+	public HashMap<String, List<ItemStack[]>> offers = new HashMap<String, List<ItemStack[]>>();
 
 	public MarketData() {
 		super("hfr_market");
@@ -22,110 +32,44 @@ public class MarketData extends WorldSavedData {
 	public MarketData(String name) {
 		super(name);
 	}
-	
-	public static MarketData getData(World worldObj) {
 
-		MarketData data = (MarketData)worldObj.perWorldStorage.loadData(MarketData.class, "hfr_market");
-	    if(data == null) {
-	        worldObj.perWorldStorage.setData("hfr_market", new MarketData());
-	        
-	        data = (MarketData)worldObj.perWorldStorage.loadData(MarketData.class, "hfr_market");
-	    }
-	    
-	    return data;
+	public static MarketData getData(World world) {
+		MarketData data = (MarketData) world.perWorldStorage.loadData(MarketData.class, "hfr_market");
+		if (data == null) {
+			data = new MarketData();
+			world.perWorldStorage.setData("hfr_market", data);
+			data.loadFromFile();
+		}
+		return data;
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
-		
-		int count = nbt.getInteger("count");
-		
-		readMarkets(nbt, count);
-	}
-	
-	public void readMarkets(NBTTagCompound nbt, int count) {
-		
-		for(int index = 0; index < count; index++) {
-			
-			String name = nbt.getString("market_" + index);
-			int offerCount = nbt.getInteger("offercount_" + index);
-			
-			for(int off = 0; off < offerCount; off++)
-				readOffers(nbt, name, off);
-		}
-	}
-	
-	public void readMarketFromPacket(NBTTagCompound nbt) {
-			
-		String name = nbt.getString("market");
-		int offerCount = nbt.getInteger("offercount");
-
-		for (int off = 0; off < offerCount; off++)
-			readOffers(nbt, name, off);
-	}
-	
-	public void readOffers(NBTTagCompound nbt, String name, int index) {
-
-		ItemStack[] slots = new ItemStack[4];
-		NBTTagList list = nbt.getTagList("items" + name + index, 10);
-		
-		for (int j = 0; j < list.tagCount(); j++) {
-			NBTTagCompound nbt1 = list.getCompoundTagAt(j);
-			byte b0 = nbt1.getByte("slot" + index);
-			if (b0 >= 0 && b0 < slots.length) {
-				slots[b0] = ItemStack.loadItemStackFromNBT(nbt1);
-			}
-		}
-		
-		List<ItemStack[]> offers = this.offers.get(name);
-		
-		if(offers == null)
-			offers = new ArrayList();
-		
-		offers.add(slots);
-		this.offers.put(name, offers);
+		loadFromFile();
 	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound nbt) {
-		
-		nbt.setInteger("count", offers.size());
-		
-		writeMarkets(nbt);
-	}
-
-	public void writeMarkets(NBTTagCompound nbt) {
-		
-		int index = 0;
-		
-		for(Entry<String, List<ItemStack[]>> entry : offers.entrySet()) {
-			
-			nbt.setString("market_" + index, entry.getKey());
-			nbt.setInteger("offercount_" + index, entry.getValue().size());
-			
-			writeOffers(nbt, entry.getKey(), entry.getValue());
-			
-			index++;
-		}
+		saveToFile();
 	}
 
 	public void writeMarketFromName(NBTTagCompound nbt, String name) {
-		
+
 		List<ItemStack[]> market = this.offers.get(name);
-		
+
 		if(market == null)
 			return;
-			
+
 		nbt.setString("market", name);
 		nbt.setInteger("offercount", market.size());
-		
+
 		writeOffers(nbt, name, market);
 	}
-	
+
 	public void writeOffers(NBTTagCompound nbt, String name, List<ItemStack[]> offers) {
-		
-		for(int index = 0; index < offers.size(); index++) {
-			
+
+		for (int index = 0; index < offers.size(); index++) {
+
 			NBTTagList list = new NBTTagList();
 			ItemStack[] offer = offers.get(index);
 
@@ -136,9 +80,49 @@ public class MarketData extends WorldSavedData {
 					offer[i].writeToNBT(nbt1);
 					list.appendTag(nbt1);
 				}
+
 			}
-			
-			nbt.setTag("items" + name + index, list);
+		}
+	}
+
+	private void loadFromFile() {
+		File file = new File(FILE_NAME);
+		if (file.exists()) {
+			FileReader reader = null;
+			try {
+				reader = new FileReader(file);
+				Type type = new TypeToken<HashMap<String, List<ItemStack[]>>>() {}.getType();
+				this.offers = GSON.fromJson(reader, type);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+
+	private void saveToFile() {
+		File file = new File(FILE_NAME);
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(file);
+			GSON.toJson(this.offers, writer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
