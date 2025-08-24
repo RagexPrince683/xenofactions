9dcfec000d7a78b37159781e09152e2101bf76e2
RagexPrince683 <yellowtoad91@gmail.com>
2025-08-24 09:59:18 -0400

    how ima be killing people if by next year this code doesnt work

--- src/main/java/com/hfr/packet/tile/OfferPacket.java
+++ src/main/java/com/hfr/packet/tile/OfferPacket.java
@@ -6,30 +6,25 @@ import java.util.List;
 import com.hfr.blocks.machine.MachineMarket;
 import com.hfr.data.MarketData;
 import com.hfr.inventory.gui.GUIMachineMarket;
-import com.hfr.packet.PacketDispatcher; // (optional)
+import com.hfr.packet.PacketDispatcher;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
-import cpw.mods.fml.relauncher.Side;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 /**
- * OfferPacket - send a market name and optional NBT payload.
+ * OfferPacket - carries a name + optional NBT.
  * Register ServerHandler for Side.SERVER and ClientHandler for Side.CLIENT.
  */
 public class OfferPacket implements IMessage {
-
 	private String name;
 	private NBTTagCompound nbt;
 
-	// required empty ctor
 	public OfferPacket() {}
 
 	public OfferPacket(String name, NBTTagCompound nbt) {
@@ -39,16 +34,14 @@ public class OfferPacket implements IMessage {
 
 	@Override
 	public void fromBytes(ByteBuf buf) {
-		// read name
 		this.name = ByteBufUtils.readUTF8String(buf);
-		// read optional NBT (null if none)
-		this.nbt = ByteBufUtils.readTag(buf);
+		this.nbt = ByteBufUtils.readTag(buf); // may be null
 	}
 
 	@Override
 	public void toBytes(ByteBuf buf) {
 		ByteBufUtils.writeUTF8String(buf, name == null ? "" : name);
-		ByteBufUtils.writeTag(buf, nbt); // handles null
+		ByteBufUtils.writeTag(buf, nbt); // writes null safely
 	}
 
 	// -------------------------
@@ -57,24 +50,17 @@ public class OfferPacket implements IMessage {
 	public static class ServerHandler implements IMessageHandler<OfferPacket, IMessage> {
 		@Override
 		public IMessage onMessage(final OfferPacket msg, final MessageContext ctx) {
-			// NOTE: in many 1.7.10 setups SimpleNetworkWrapper will call this on the main thread,
-			// but if you ever see thread issues, schedule this on the server main thread.
 			try {
-				// Example: client asked server for offers with name -> server should load data + reply
-				MarketData.loadMarketData(); // server-side load
+				// In 1.7.10 this runs on the server main thread already; do server-side work directly.
+				MarketData.loadMarketData();
 				List<ItemStack[]> offers = MarketData.getOffers(msg.name);
 				if (offers == null) offers = new ArrayList<ItemStack[]>();
 
-				// If MachineMarket is server-side data you want to set:
 				MachineMarket.name = msg.name;
 
-				// If you want to send the offers back to the client, serialize them into NBT and send:
-				// (You need to implement MarketData.writeOffersToNBT if you want full serialization)
+				// If you want to reply with data serialized to NBT, build response NBT and send:
 				NBTTagCompound response = new NBTTagCompound();
-				// TODO: serialize offers into 'response' if desired
-				// e.g. MarketData.writeOffersToNBT(response, offers);
-
-				// Send back to the requesting player:
+				// TODO: serialize offers into 'response' if desired (MarketData.writeOffersToNBT)
 				EntityPlayerMP player = ctx.getServerHandler().playerEntity;
 				PacketDispatcher.wrapper.sendTo(new OfferPacket(msg.name, response), player);
 
@@ -91,30 +77,24 @@ public class OfferPacket implements IMessage {
 	public static class ClientHandler implements IMessageHandler<OfferPacket, IMessage> {
 		@Override
 		public IMessage onMessage(final OfferPacket msg, final MessageContext ctx) {
-			// Schedule on client main thread
-			Minecraft.getMinecraft().addScheduledTask(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						// If server sent an NBT payload, parse it; otherwise reload MarketData locally.
-						if (msg.nbt != null && !msg.nbt.hasNoTags()) {
-							// TODO: parse msg.nbt into MarketData / offers if you serialized them server-side
-							// e.g. MarketData.readOffersFromNBT(msg.nbt);
-						} else {
-							MarketData.loadMarketData(); // fallback: client loads JSON
-						}
-
-						List<ItemStack[]> offers = MarketData.getOffers(msg.name);
-						if (offers == null) offers = new ArrayList<ItemStack[]>();
-
-						MachineMarket.name = msg.name;
-						GUIMachineMarket.offers = offers;
-
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+			try {
+				// In 1.7.10 this runs on the client thread already; run client GUI updates directly.
+				if (msg.nbt != null && !msg.nbt.hasNoTags()) {
+					//MarketData
+					// TODO: if server serialized offers into NBT, read them here (MarketData.readOffersFromNBT)
+				} else {
+					MarketData.loadMarketData(); // fallback if client keeps a JSON copy
 				}
-			});
+
+				List<ItemStack[]> offers = MarketData.getOffers(msg.name);
+				if (offers == null) offers = new ArrayList<ItemStack[]>();
+
+				MachineMarket.name = msg.name;
+				GUIMachineMarket.offers = offers;
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			return null;
 		}
 	}
