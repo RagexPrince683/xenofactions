492b8f93e34ae6b2889d808768785b9f83850d73
yes pocketwatch my code, good boy <49804375+RagexPrince683@users.noreply.github.com>
2026-08-22 19:53:19 -0400

    Fix Wall Art block and configuration GUI

--- src/main/java/com/hfr/inventory/gui/GuiWallArt.java
+++ src/main/java/com/hfr/inventory/gui/GuiWallArt.java
@@ -1,3 +1,140 @@
 package com.hfr.inventory.gui;
-import com.hfr.packet.PacketDispatcher;import com.hfr.packet.wallart.WallArtConfigurePacket;import com.hfr.tileentity.TileEntityWallImage;import cpw.mods.fml.relauncher.Side;import cpw.mods.fml.relauncher.SideOnly;import net.minecraft.client.gui.*;import org.lwjgl.input.Keyboard;
-@SideOnly(Side.CLIENT) public final class GuiWallArt extends GuiScreen{private final int dimension,x,y,z;private int width,height;private GuiTextField url;public GuiWallArt(TileEntityWallImage t){dimension=t.getWorldObj().provider.dimensionId;x=t.xCoord;y=t.yCoord;z=t.zCoord;width=t.getDisplayWidth();height=t.getDisplayHeight();}public void initGui(){Keyboard.enableRepeatEvents(true);url=new GuiTextField(fontRendererObj,this.width/2-120,this.height/2-35,240,20);url.setMaxStringLength(2048);url.setFocused(true);buttonList.add(new GuiButton(1,this.width/2-120,this.height/2,55,20,"W: "+width));buttonList.add(new GuiButton(2,this.width/2-60,this.height/2,55,20,"H: "+height));buttonList.add(new GuiButton(0,this.width/2+5,this.height/2,115,20,"Download"));}protected void actionPerformed(GuiButton b){if(b.id==1){width=width%5+1;b.displayString="W: "+width;}else if(b.id==2){height=height%5+1;b.displayString="H: "+height;}else if(b.id==0&&!url.getText().isEmpty()){PacketDispatcher.wrapper.sendToServer(new WallArtConfigurePacket(dimension,x,y,z,width,height,url.getText()));mc.displayGuiScreen(null);}}protected void keyTyped(char c,int k){if(k==Keyboard.KEY_ESCAPE){mc.displayGuiScreen(null);return;}url.textboxKeyTyped(c,k);}protected void mouseClicked(int x,int y,int b){super.mouseClicked(x,y,b);url.mouseClicked(x,y,b);}public void drawScreen(int mx,int my,float f){drawDefaultBackground();drawCenteredString(fontRendererObj,"Wall Art (HTTPS URL)",width/2,height/2-60,0xffffff);url.drawTextBox();super.drawScreen(mx,my,f);}public void onGuiClosed(){Keyboard.enableRepeatEvents(false);}}
+
+import com.hfr.packet.PacketDispatcher;
+import com.hfr.packet.wallart.WallArtConfigurePacket;
+import com.hfr.tileentity.TileEntityWallImage;
+import com.hfr.wallart.WallArtConstants;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiTextField;
+
+import org.lwjgl.input.Keyboard;
+
+@SideOnly(Side.CLIENT)
+public final class GuiWallArt extends GuiScreen {
+    private static final int BUTTON_DOWNLOAD = 0;
+    private static final int BUTTON_WIDTH = 1;
+    private static final int BUTTON_HEIGHT = 2;
+    private static final int CONTROL_WIDTH = 240;
+
+    private final int dimension;
+    private final int controllerX;
+    private final int controllerY;
+    private final int controllerZ;
+
+    private int displayWidth;
+    private int displayHeight;
+    private GuiTextField urlField;
+    private String statusMessage = "";
+
+    public GuiWallArt(TileEntityWallImage tile) {
+        dimension = tile.getWorldObj().provider.dimensionId;
+        controllerX = tile.xCoord;
+        controllerY = tile.yCoord;
+        controllerZ = tile.zCoord;
+        displayWidth = tile.getDisplayWidth();
+        displayHeight = tile.getDisplayHeight();
+    }
+
+    @Override
+    public void initGui() {
+        Keyboard.enableRepeatEvents(true);
+        buttonList.clear();
+
+        String existingUrl = urlField == null ? "" : urlField.getText();
+        int left = this.width / 2 - CONTROL_WIDTH / 2;
+        int top = this.height / 2 - 70;
+
+        urlField = new GuiTextField(fontRendererObj, left, top + 38, CONTROL_WIDTH, 20);
+        urlField.setMaxStringLength(WallArtConstants.MAX_URL_BYTES);
+        urlField.setText(existingUrl);
+        urlField.setFocused(true);
+
+        buttonList.add(new GuiButton(BUTTON_WIDTH, left, top + 78, 115, 20,
+            widthButtonText()));
+        buttonList.add(new GuiButton(BUTTON_HEIGHT, left + 125, top + 78, 115, 20,
+            heightButtonText()));
+        buttonList.add(new GuiButton(BUTTON_DOWNLOAD, this.width / 2 - 60, top + 108,
+            120, 20, "Download"));
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) {
+        if (button.id == BUTTON_WIDTH) {
+            displayWidth = displayWidth % WallArtConstants.MAX_SIZE + 1;
+            button.displayString = widthButtonText();
+        } else if (button.id == BUTTON_HEIGHT) {
+            displayHeight = displayHeight % WallArtConstants.MAX_SIZE + 1;
+            button.displayString = heightButtonText();
+        } else if (button.id == BUTTON_DOWNLOAD) {
+            String enteredUrl = urlField.getText().trim();
+            if (enteredUrl.length() == 0) {
+                statusMessage = "Enter an HTTPS image URL.";
+                return;
+            }
+
+            PacketDispatcher.wrapper.sendToServer(new WallArtConfigurePacket(
+                dimension,
+                controllerX,
+                controllerY,
+                controllerZ,
+                displayWidth,
+                displayHeight,
+                enteredUrl));
+            mc.displayGuiScreen(null);
+        }
+    }
+
+    @Override
+    protected void keyTyped(char character, int keyCode) {
+        if (keyCode == Keyboard.KEY_ESCAPE) {
+            mc.displayGuiScreen(null);
+            return;
+        }
+        urlField.textboxKeyTyped(character, keyCode);
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        urlField.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
+    public void updateScreen() {
+        urlField.updateCursorCounter();
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
+        int top = this.height / 2 - 70;
+        drawCenteredString(fontRendererObj, "Wall Art", this.width / 2, top, 0xFFFFFF);
+        drawString(fontRendererObj, "HTTPS Image URL:", this.width / 2 - CONTROL_WIDTH / 2,
+            top + 25, 0xA0A0A0);
+        urlField.drawTextBox();
+        drawCenteredString(fontRendererObj, "Display Size:", this.width / 2, top + 66,
+            0xA0A0A0);
+        if (statusMessage.length() > 0) {
+            drawCenteredString(fontRendererObj, statusMessage, this.width / 2, top + 133,
+                0xFF8080);
+        }
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    public void onGuiClosed() {
+        Keyboard.enableRepeatEvents(false);
+    }
+
+    private String widthButtonText() {
+        return "Width: " + displayWidth;
+    }
+
+    private String heightButtonText() {
+        return "Height: " + displayHeight;
+    }
+}
